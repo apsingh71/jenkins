666eadd9632d01941e4ab62747342125a09db6a9
AP <ap_singh71@hotmail.com>
2022-01-16 16:26:20 -0800

    add sys.argv

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,10 @@
+import sys
+
 def main():
   print('From docker container running python3 in dev branch')
   print('Hello world')
 
 if __name__ == '__main__':
+  print('Argument: ', sys.argv[1])
   main()
 
